f8aed892fbffce1f563fe4cf41bf6fc988cde372
daoducnha <daoducnha29493@gmail.com>
2021-04-20 06:33:30 +0700

    update code

--- todo_crud.ex
+++ todo_crud.ex
@@ -3,15 +3,6 @@ defmodule TodoList do
 
   def new(), do: %TodoList{}
 
-  def new(entries \\ []) do
-    Enum.reduce(
-      entries,
-      %TodoList{},
-      add_entry(&2, &1)
-      end
-    )
-  end
-
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.auto_id)
 
@@ -31,6 +22,10 @@ defmodule TodoList do
     |> Enum.map(fn {_, entry} -> entry end)
   end
 
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
   def update_entry(todo_list, entry_id, update_fun) do
     case Map.fetch(todo_list.entries, entry_id) do
       :error ->
@@ -39,11 +34,33 @@ defmodule TodoList do
       {:ok, old_entry} ->
         new_entry = update_fun.(old_entry)
         new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
-        %Todo{todo_list | entries: new_entries}
+        %TodoList{todo_list | entries: new_entries}
     end
   end
-end
 
-defmodule TodoList.CsvImporter do
+  def delete_entry(todo_list, entry_id) do
 
+
+    new_entries = todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.id != id end)
+    |> Enum.map({_, entry} -> entry)
+
+    %TodoList{todo_list | entries: new_entries}
+  end
+
+  def test() do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: ~D[2018-12-19], id: 1, title: "Dentist"})
+      |> TodoList.add_entry(%{date: ~D[2018-12-20], id: 2, title: "Shopping"})
+      |> TodoList.add_entry(%{date: ~D[2018-12-19], id: 3, title: "Movies"})
+
+    # todo_list.update_entry(
+    #   todo_list,
+    #   1,
+    #   &Map.put(&1, :date, ~D[2018-12-20])
+    # )
+
+    IO.puts(todo_list)
+  end
 end
